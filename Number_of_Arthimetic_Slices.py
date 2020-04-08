eda10b491803172cb285840faefbb88b953189e8
farazlfc <31564869+farazlfc@users.noreply.github.com>
2020-04-09 02:23:30 +0530

    Update Number_of_Arthimetic_Slices.py

--- Number_of_Arthimetic_Slices.py
+++ Number_of_Arthimetic_Slices.py
@@ -6,21 +6,11 @@ class Solution:
             return 0
         a,b = min(A),max(A)
         diff = b - a
-        def binomialCoef(n, k): 
-            C = [[0 for x in range(k+1)] for x in range(n+1)] 
-            for i in range(n+1): 
-                for j in range(min(i, k)+1): 
-                    # Base Cases 
-                    if j == 0 or j == i: 
-                        C[i][j] = 1
-                    else: 
-                        C[i][j] = C[i-1][j-1] + C[i-1][j] 
-            return C[n][k] 
             
         if diff == 0:
-            return (2**n - 1 - n - binomialCoef(n,2))
+            return (2**n - 1 - n - (n*(n-1))//2)
         dp = [defaultdict(list) for _ in range(n)]  #0 for start
-        
+        count = 0
         for i in range(1,n):
             for j in range(i):
                 diff = A[i] - A[j]
@@ -29,11 +19,7 @@ class Solution:
                 else:
                     dp[i][diff].append(1)
                     for el in dp[j][diff]:
-                        dp[i][diff].append(el+1)
-        count = 0
-        for index in dp:
-            for key in index.keys():
-                for element in index[key]:
-                    if element > 1:
                         count+=1
+                        dp[i][diff].append(el+1)
         return count
+                
