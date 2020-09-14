9fc087aaeb6b6f8b57b049ef92e9321164e538f5
farazlfc <31564869+farazlfc@users.noreply.github.com>
2020-09-14 20:02:14 +0530

    Update Find_XOR.py

--- Find_XOR.py
+++ Find_XOR.py
@@ -1,22 +1,19 @@
 for _ in range(int(input())):
-		n = int(input())
-		ans1 = 0;
-		print("1 1048576")
-		sum = int(input())
-		sum -= n*1048576;
-		if sum%2: 
-      ans1 = 1;
-		for(int i = 1; i <= 19; i++){
-			cout << 1 << " " << (1 << i) << endl;
-			ll sum1;
-			cin >> sum1;
-			ll diff = sum - sum1;
-			diff = diff/(1LL << i);
-			ll on = (n + diff)/2;
-			if(on%2 == 1) ans1 |= (1 << i);
-		}
-		cout << 2 << " " << ans1 << endl;
-		int j;
-		cin >> j;
-		assert(j == 1);
-	}
+	n = int(input())
+	ans1 = 0;
+	print("1 1048576")
+	sum = int(input())
+	sum -= n*1048576;
+	ans1 = 0;
+	if sum%2: 
+      		ans1 = 1;
+	for i in range(1,20):
+		print("1 " + str((1 << i)))
+		sum1 = int(input())
+		diff = sum - sum1;
+		diff = diff/(1 << i);
+		on = (n + diff)/2;
+		if on%2 == 1:  #odd number of bits set!
+			ans1 |= (1 << i);
+	print("2 " + str(ans1))
+	_ = int(input());
